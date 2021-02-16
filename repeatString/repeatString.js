635f6c8c1838ee2f66a265c31ea0ee283af281a3
Eldian Restorationist <eldianrestorationist@gmail.com>
2021-02-16 11:52:35 -0600

    add variables, function body, function call

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    for (i = 0; i < num; i++) {
+        return text;
+    }
 }
-
+repeatString('hey', 3);
 module.exports = repeatString
