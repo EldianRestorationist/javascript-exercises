e525b5f94e65f4dc30c4bdb24f70a8f26ee68f41
Eldian Restorationist <eldianrestorationist@gmail.com>
2021-02-16 15:35:38 -0600

    incomplete

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num) {
+    let index = array.indexOf(num);
+    array.splice(index, 1);
 
+    return array;
 }
 
 module.exports = removeFromArray
