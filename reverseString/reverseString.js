11f91aa54938b8d366b2efe14565cc87633a2ffc
Eldian Restorationist <eldianrestorationist@gmail.com>
2021-02-16 15:39:23 -0600

    finished

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stringText) {
+    stringReverse = '';
+    let i = stringText.length;
+    while (i >= 0) {
+        stringReverse += stringText.charAt(i);
+        i--;
+    }
+    return stringReverse;
 }
 
 module.exports = reverseString
